f9d3d0a63acc608e94e9383ac0f37a5cd8ccb015
nabeel929 <57416512+nabeel929@users.noreply.github.com>
2026-02-10 17:15:29 +0500

    worked on Issues

--- server/index.js
+++ server/index.js
@@ -11,12 +11,13 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use('/api',router)
 
 const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    // await mongoose.connect(process.env.MONGO_URI);
     console.log(`App is connected to the database.`);
   } catch (error) {
     console.error(`Error connecting to DB: ${error.message}`);
